78a95962d3927279c8773dd1be4d4944278b2deb
sameer pokhre <sameeerpokhrel@gmail.com>
2022-11-06 01:24:44 +0545

    fix date & screenshoot

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter polkit-gnome lightdm-gtk-greeter-settings i3 lxappearance nitrogen picom dmenu rofi pcmanfm flatpak pacman-contrib pavucontro archlinux-wallpaper  ttf-font-awesome xfce4-terminal materia-gtk-theme
+sudo pacman -S --noconfirm xorg xclip maim lightdm lightdm-gtk-greeter polkit-gnome lightdm-gtk-greeter-settings i3 lxappearance nitrogen picom dmenu rofi pcmanfm flatpak pacman-contrib pavucontro archlinux-wallpaper  ttf-font-awesome xfce4-terminal materia-gtk-theme
 sudo pacman -R --noconfirm i3lock
 pikaur -S --noconfirm bumblebee-status betterlockscreen
 sudo systemctl enable lightdm
